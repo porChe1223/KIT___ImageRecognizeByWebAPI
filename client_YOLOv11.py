8785ac417708dc063410863e39396cb740c79b2b
porChe1223 <houshinri@gmail.com>
2024-11-18 16:06:47 +0900

    change: データのパス

--- client_YOLOv11.py
+++ client_YOLOv11.py
@@ -5,7 +5,7 @@ server_url = 'http://127.0.0.1:60003/server_YOLOv11'
 
 # 送信データ
 post_data = {
-    'image': '/home/porche1223/3-3___WebAPI画像処理システム/WebAPI_Dataset/children_playing_football.jpeg'
+    'image': '/home/porche1223/3-3___WebAPI画像処理システム/ImageRecognizeByWebAPI/WebAPI_Dataset/children_playing_football.jpeg'
 }
 # POSTリクエストの送信
 response = requests.post(server_url, json=post_data)
